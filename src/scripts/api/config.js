9bac009b45aef0bfa9f4c2fc68347d651dc7dd3a
Azkal Azkiya <azkalazkiya940@gmail.com>
2024-06-10 20:11:30 +0700

    Update config.js

--- src/scripts/api/config.js
+++ src/scripts/api/config.js
@@ -51,7 +51,7 @@ async function fetchDataAll() {
       listBatik.batik1 = gallery;
 
       const colDiv = document.createElement('div');
-      colDiv.className = 'col-4';
+      colDiv.className = 'col-12 col-sm-6 col-md-4 d-flex justify-content-center pb-5';
       colDiv.appendChild(listBatik);
 
       galleryContainer.appendChild(colDiv);
